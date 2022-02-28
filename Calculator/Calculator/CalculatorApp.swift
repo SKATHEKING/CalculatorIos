5a70c2eb0a7f6cdb1c313833e657f29990e93bb3
SKATHEKING <46651755+SKATHEKING@users.noreply.github.com>
2022-02-28 12:46:40 +0000

    Add files via upload

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mateus Goncalves De Ouro on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
